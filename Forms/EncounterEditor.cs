37fba500a79639a41bf5db8700e56dff2de6551b
Ben J. Ayers <ben5292001@me.com>
2026-09-13 18:53:09 -0400

    WIP do not use - broken overwrite

--- Forms/EncounterEditor.cs
+++ Forms/EncounterEditor.cs
@@ -1,13 +1,9 @@
-﻿using NewEditor.Data;
+using NewEditor.Data;
 using NewEditor.Data.NARCTypes;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NewEditor.Forms
@@ -22,277 +18,71 @@ namespace NewEditor.Forms
         public EncounterEditor()
         {
             InitializeComponent();
+            AddPlannerImportButton();
 
             encounterRouteNameDropdown.Items.AddRange(encounterNarc.encounterPools.ToArray());
             encounterPokemonNameDropDown.Items.AddRange(textNARC.textFiles[VersionConstants.PokemonNameTextFileID].text.ToArray());
         }
 
-        private void encounterRouteNameDropdown_SelectedIndexChanged(object sender, EventArgs ev)
+        private void AddPlannerImportButton()
         {
-            if (encounterRouteNameDropdown.SelectedItem is EncounterEntry e)
-            {
-                routeEncounterTypesPanel.Enabled = true;
-                encounterGroupListBox.Enabled = true;
-                addEncounterGroupButton.Enabled = true;
-                removeEncounterGroupButton.Enabled = true;
-                encounterPokemonNameDropDown.Enabled = true;
-                encounterGroupFormNumber.Enabled = true;
-                encounterGroupMinLvNumber.Enabled = true;
-                encounterGroupMaxLvNumber.Enabled = true;
-                encounterGroupRateNumber.Enabled = true;
-                encounterSlotApplyButton.Enabled = true;
-                copyGrassToDarkGrassButton.Enabled = true;
-                copyEncounterGroupButton.Enabled = true;
-                encounterGroupApplyRouteButton.Enabled = true;
-                toggleSeasonsButton.Enabled = true;
-
-                if (e.season == -1) toggleSeasonsButton.Text = "Add Seasons";
-                else toggleSeasonsButton.Text = "Remove Seasons";
-
-                e.EncounterSlotsToGroups();
-                SetupEncounterListBox(true);
-            }
-            else
-            {
-                routeEncounterTypesPanel.Enabled = false;
-                encounterGroupListBox.Enabled = false;
-                addEncounterGroupButton.Enabled = false;
-                removeEncounterGroupButton.Enabled = false;
-                encounterPokemonNameDropDown.Enabled = false;
-                encounterGroupFormNumber.Enabled = false;
-                encounterGroupMinLvNumber.Enabled = false;
-                encounterGroupMaxLvNumber.Enabled = false;
-                encounterGroupRateNumber.Enabled = false;
-                encounterSlotApplyButton.Enabled = false;
-                copyGrassToDarkGrassButton.Enabled = false;
-                copyEncounterGroupButton.Enabled = false;
-                encounterGroupApplyRouteButton.Enabled = false;
-                toggleSeasonsButton.Enabled = false;
-            }
+            var importPlannerJsonButton = new Button();
+            importPlannerJsonButton.Location = new Point(370, 10);
+            importPlannerJsonButton.Name = "importPlannerJsonButton";
+            importPlannerJsonButton.Size = new Size(170, 28);
+            importPlannerJsonButton.Text = "Import Planner JSON...";
+            importPlannerJsonButton.UseVisualStyleBackColor = true;
+            importPlannerJsonButton.Click += ImportPlannerJsonButton_Click;
+            Controls.Add(importPlannerJsonButton);
         }
 
-        private void SetupEncounterListBox(bool redoListBox)
+        private void ImportPlannerJsonButton_Click(object sender, EventArgs e)
         {
-            if (encounterRouteNameDropdown.SelectedItem is EncounterEntry e)
+            if (MainEditor.RomType != RomType.BW2)
             {
-                List<RadioButton> radios = new List<RadioButton>()
-                {
-                    radioButton1,
-                    radioButton2,
-                    radioButton3,
-                    radioButton4,
-                    radioButton5,
-                    radioButton6,
-                    radioButton7
-                };
-
-                int selectedType = 0;
-                for (int i = 0; i < 7; i++) if (radios[i].Checked) selectedType = i;
-
-                if (redoListBox)
-                {
-                    encounterGroupListBox.Items.Clear();
-                    if (selectedType <= 2) foreach (EncounterSlot slot in e.groupedLandSlots[selectedType])
-                        {
-                            encounterGroupListBox.Items.Add(slot);
-                        }
-                    else foreach (EncounterSlot slot in e.groupedWaterSlots[selectedType - 3])
-                        {
-                            encounterGroupListBox.Items.Add(slot);
-                        }
-                    encounterGroupListBox.SelectedIndex = 0;
-                }
-
-                if (encounterGroupListBox.SelectedItem is EncounterSlot encSlot)
-                {
-                    encounterPokemonNameDropDown.SelectedIndex = encSlot.pokemonID;
-                    encounterGroupFormNumber.Value = encSlot.pokemonForm;
-                    encounterGroupMinLvNumber.Value = Math.Max(encSlot.minLevel, (byte)1);
-                    encounterGroupMaxLvNumber.Value = Math.Max(encSlot.maxLevel, (byte)1);
-                    encounterGroupRateNumber.Value = encSlot.rate;
-                }
-            }
-        }
-
-        private void encounterGroupListBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            SetupEncounterListBox(false);
-        }
-
-        private void ChangeRouteEncounterType(object sender, EventArgs e)
-        {
-            SetupEncounterListBox(true);
-        }
-
-        private void encounterGroupApplyRouteButton_Click(object sender, EventArgs e)
-        {
-            if (encounterRouteNameDropdown.SelectedItem is EncounterEntry route)
-            {
-                List<RadioButton> radios = new List<RadioButton>()
-                {
-                    radioButton1,
-                    radioButton2,
-                    radioButton3,
-                    radioButton4,
-                    radioButton5,
-                    radioButton6,
-                    radioButton7
-                };
-
-                int selectedType = 0;
-                for (int i = 0; i < 7; i++) if (radios[i].Checked) selectedType = i;
-
-                if (selectedType <= 2)
-                {
-                    route.groupedLandSlots[selectedType].Clear();
-                    foreach (EncounterSlot slot in encounterGroupListBox.Items) route.groupedLandSlots[selectedType].Add(slot);
-                }
-                else
-                {
-                    route.groupedWaterSlots[selectedType - 3].Clear();
-                    foreach (EncounterSlot slot in encounterGroupListBox.Items) route.groupedWaterSlots[selectedType - 3].Add(slot);
-                }
-                route.ApplyData();
-
-                statusText.Text = "Saved encounter data - " + DateTime.Now.StatusText();
-            }
-        }
-
-        private void encounterSlotApplyButton_Click(object sender, EventArgs e)
-        {
-            if (encounterGroupListBox.SelectedItem is EncounterSlot slot)
-            {
-                slot.pokemonID = (short)encounterPokemonNameDropDown.SelectedIndex;
-                slot.pokemonForm = (int)encounterGroupFormNumber.Value;
-                slot.minLevel = (byte)encounterGroupMinLvNumber.Value;
-                slot.maxLevel = (byte)encounterGroupMaxLvNumber.Value;
-                slot.rate = (int)encounterGroupRateNumber.Value;
+                MessageBox.Show("Planner JSON import currently supports Black 2 / White 2 encounter pools.");
+                return;
             }
 
-            int storeIndex = encounterGroupListBox.SelectedIndex;
-            SetupEncounterListBox(true);
-            encounterGroupListBox.SelectedIndex = Math.Min(storeIndex, encounterGroupListBox.Items.Count - 1);
-        }
-
-        private void copyEncounterGroupButton_Click(object sender, EventArgs e)
-        {
-            encountersClipboard = new List<EncounterSlot>();
-            foreach (EncounterSlot slot in encounterGroupListBox.Items) encountersClipboard.Add(slot.Clone());
-            pasteEncounterGroupButton.Enabled = true;
-        }
-
-        private void pasteEncounterGroupButton_Click(object sender, EventArgs e)
-        {
-            encounterGroupListBox.Items.Clear();
-            foreach (EncounterSlot slot in encountersClipboard) encounterGroupListBox.Items.Add(slot.Clone());
-        }
-
-        private void addEncounterGroupButton_Click(object sender, EventArgs e)
-        {
-            if (encounterRouteNameDropdown.SelectedItem is EncounterEntry route)
+            using (OpenFileDialog dlg = new OpenFileDialog())
             {
-                int selectedType = 0;
-                for (int i = 6; i >= 0; i--) if (((RadioButton)routeEncounterTypesPanel.Controls[i]).Checked) selectedType = 6 - i;
+                dlg.Filter = "Planner JSON (*.json)|*.json|All files (*.*)|*.*";
+                dlg.Title = "Import encounter planner JSON";
+                if (dlg.ShowDialog() != DialogResult.OK) return;
 
-                List<EncounterSlot> slotList = new List<EncounterSlot>();
-                if (selectedType <= 2) slotList = route.groupedLandSlots[selectedType];
-                else slotList = route.groupedWaterSlots[selectedType - 3];
-
-                if (slotList.Count < 12) slotList.Add(new EncounterSlot(1, 0, 1, 1, 0));
-                SetupEncounterListBox(true);
-                encounterGroupListBox.SelectedIndex = slotList.Count - 1;
-            }
-        }
-
-        private void removeEncounterGroupButton_Click(object sender, EventArgs e)
-        {
-            if (encounterRouteNameDropdown.SelectedItem is EncounterEntry route)
-            {
-                int selectedType = 0;
-                for (int i = 6; i >= 0; i--) if (((RadioButton)routeEncounterTypesPanel.Controls[i]).Checked) selectedType = 6 - i;
-
-                List<EncounterSlot> slotList = new List<EncounterSlot>();
-                if (selectedType <= 2) slotList = route.groupedLandSlots[selectedType];
-                else slotList = route.groupedWaterSlots[selectedType - 3];
-
-                if (slotList.Count > 1) slotList.RemoveAt(encounterGroupListBox.SelectedIndex);
-                int storeIndex = encounterGroupListBox.SelectedIndex;
-                SetupEncounterListBox(true);
-                encounterGroupListBox.SelectedIndex = Math.Min(storeIndex, slotList.Count - 1);
-            }
-        }
-
-        private void ViewPokemonButton(object sender, EventArgs e)
-        {
-            Program.main.OpenPokemonEditor(sender, e);
-            if (MainEditor.pokemonEditor != null)
-            {
-                if (MainEditor.pokemonDataNarc.pokemon.Exists(p => p.nameID == encounterPokemonNameDropDown.SelectedIndex)) MainEditor.pokemonEditor.pokemonNameDropdown.SelectedItem = MainEditor.pokemonDataNarc.pokemon.First(p => p.nameID == encounterPokemonNameDropDown.SelectedIndex);
-                else MessageBox.Show("Could not find the pokemon");
-            }
-        }
-
-        private void copyGrassToDarkGrassButton_Click(object sender, EventArgs e)
-        {
-            if (encounterRouteNameDropdown.SelectedItem is EncounterEntry route)
-            {
-                route.groupedLandSlots[1].Clear();
-                foreach (EncounterSlot slot in route.groupedLandSlots[0])
+                try
                 {
-                    EncounterSlot s = slot.Clone();
-                    s.minLevel = (byte)Math.Min((int)Math.Ceiling(s.minLevel * 1.1f), 100);
-                    s.maxLevel = (byte)Math.Min((int)Math.Ceiling(s.maxLevel * 1.1f), 100);
-                    route.groupedLandSlots[1].Add(s);
-                }
+                    string text = System.IO.File.ReadAllText(dlg.FileName);
+                    var result = PlannerEncounterImport.Import(text, encounterNarc);
 
-                statusText.Text = "Copied grass encounters to dark grass - " + DateTime.Now.StatusText();
-            }
-        }
-
-        private void KeepMinLevelUnderMaxLevel(object sender, EventArgs e)
-        {
-            if (encounterGroupMaxLvNumber.Value < encounterGroupMinLvNumber.Value) encounterGroupMinLvNumber.Value = encounterGroupMaxLvNumber.Value;
-        }
-
-        private void KeepMaxLevelOverMinLevel(object sender, EventArgs e)
-        {
-            if (encounterGroupMinLvNumber.Value > encounterGroupMaxLvNumber.Value) encounterGroupMaxLvNumber.Value = encounterGroupMinLvNumber.Value;
-        }
-
-        private void toggleSeasonsButton_Click(object sender, EventArgs e)
-        {
-            if (encounterRouteNameDropdown.SelectedItem is EncounterEntry route)
-            {
-                if (encounterNarc.subEncounterPools.Contains(route))
-                {
-                    MessageBox.Show("Please select the Spring entry for this route");
-                    return;
-                }
-
-                if (route.season == 0)
-                {
-                    if (MessageBox.Show("Are you sure you want to delete the seasonal enounters for this route?", "Delete Seasons", MessageBoxButtons.YesNo) == DialogResult.No)
+                    var selected = encounterRouteNameDropdown.SelectedItem;
+                    encounterRouteNameDropdown.Items.Clear();
+                    encounterRouteNameDropdown.Items.AddRange(encounterNarc.encounterPools.ToArray());
+                    if (selected is EncounterEntry keep)
                     {
-                        return;
+                        var match = encounterNarc.encounterPools.FirstOrDefault(p => p.nameID == keep.nameID && p.season == keep.season);
+                        if (match != null) encounterRouteNameDropdown.SelectedItem = match;
                     }
-                    encounterNarc.subEncounterPools.RemoveAll(r => r.parentPool == route);
-                    route.season = -1;
-                    toggleSeasonsButton.Text = "Add Seasons";
 
-                    statusText.Text = "Removed seasons to encounter pool - " + DateTime.Now.StatusText();
+                    string skipped = result.SkippedTypes.Count > 0 ? string.Join(", ", result.SkippedTypes) : "none";
+                    string unmapped = result.UnmappedLocations.Count > 0 ? string.Join(", ", result.UnmappedLocations) : "none";
+                    string missing = result.MissingPools.Count > 0 ? string.Join(", ", result.MissingPools.Distinct()) : "none";
+                    string warns = result.Warnings.Count > 0 ? "\n\nWarnings:\n" + string.Join("\n", result.Warnings.Take(20)) : "";
+
+                    MessageBox.Show(
+                        "Wrote " + result.TablesWritten + " tables (" + result.SlotsWritten + " slots).\n\n" +
+                        "Skipped types (not in wild NARC): " + skipped + "\n" +
+                        "Unmapped locations: " + unmapped + "\n" +
+                        "Missing Frost pools: " + missing +
+                        warns,
+                        "Planner import");
+
+                    statusText.Text = "Imported planner JSON - " + DateTime.Now.StatusText();
                 }
-                else if (route.season == -1)
+                catch (Exception ex)
                 {
-                    for (int i = 1; i <= 3; i++) encounterNarc.subEncounterPools.Add(new EncounterEntry(route.bytes.ToArray()) { nameID = route.nameID, season = i, parentPool = route });
-                    route.season = 0;
-                    toggleSeasonsButton.Text = "Remove Seasons";
-
-                    statusText.Text = "Added seasons to encounter pool - " + DateTime.Now.StatusText();
+                    MessageBox.Show("Import failed:\n" + ex.Message);
                 }
-
-                encounterRouteNameDropdown.Items.Clear();
-                encounterRouteNameDropdown.Items.AddRange(encounterNarc.encounterPools.ToArray());
-                encounterRouteNameDropdown.SelectedItem = route;
             }
         }
     }
